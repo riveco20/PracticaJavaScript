2890afc957da69be88d0ab80623e015e8e3d0661
Richard Vellojin <richard.vellojin@udea.edu.co>
2021-12-28 19:07:01 -0500

    Primer tema

--- Introduccion/codigo.js
+++ Introduccion/codigo.js
@@ -1,7 +1,14 @@
 var saludo ="Hola mundo";
 
+var nombre = "Richard"
+
+var edad = 29;
+
+var profesion = "Ingeniero de materiales"
 console.log(saludo);
 
-alert(saludo);
 
-document.querySelector("body").innerHTML="hola mundo";
+
+/*alert(saludo)*/
+
+document.querySelector("body").innerHTML=saludo + " mi nombre es " + nombre + " Tengo " + edad + " años" + " y soy " + profesion ;
